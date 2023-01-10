66deefe1e589bb33de7af934fa31b0b20a7a9eec
Simon Scatton <simon.scatton@epita.fr>
2023-01-10 23:12:57 +0100

    feat(home: mails): added prologin account

--- home/email.nix
+++ home/email.nix
@@ -1,9 +1,47 @@
 { config, lib, pkgs, ... }:
 let
+  inherit (lib) mapAttrs;
+  inherit (builtins) typeOf;
   name = "Simon Scatton";
   email_perso = "simon.scatton@outlook.fr";
   email_lrde = "sscatton@lrde.epita.fr";
-  email_epita = "sscatton@epita.fr";
+  email_prologin = "simon.scatton@prologin.org";
+
+  make_mbsync_channel = patterns:
+    (if (typeOf patterns) == "list" then {
+      inherit patterns;
+    } else {
+      farPattern = patterns.far;
+      nearPattern = patterns.near;
+    }) // {
+      extraConfig = {
+        Create = "Both";
+        Expunge = "Both";
+        Remove = "None";
+        SyncState = "*";
+      };
+    };
+  make_mbsync_channels = mapAttrs (_: value: make_mbsync_channel value);
+
+  gmail_far_near_patterns = {
+    sent = {
+      far = "[Gmail]/Sent Mail";
+      near = "Sent";
+    };
+    drafts = {
+      far = "[Gmail]/Drafts";
+      near = "Drafts";
+    };
+    junk = {
+      far = "[Gmail]/Spam";
+      near = "Junk";
+    };
+    trash = {
+      far = "[Gmail]/Trash";
+      near = "Trash";
+    };
+  };
+  gmail_mbsync_channels = make_mbsync_channels gmail_far_near_patterns;
 in {
 
   programs.mbsync.enable = true;
@@ -68,6 +106,40 @@ in {
           port = 587;
           tls.enable = true;
           tls.useStartTls = true;
+        };
+      };
+
+      prologin = {
+        address = email_prologin;
+        userName = email_prologin;
+        realName = name;
+        aliases = [ "sda@prologin.org" ];
+        flavor = "plain"; # default setting
+        passwordCommand = "echo ***REMOVED***";
+        primary = false;
+        mbsync = {
+          enable = true;
+          create = "both";
+          expunge = "both";
+          groups = {
+            prologin-main.channels =
+              (make_mbsync_channels { main = [ "INBOX" "membres@" ]; })
+              // gmail_mbsync_channels;
+            prologin-info.channels =
+              make_mbsync_channels { info = [ "info@" "info@gcc" ]; };
+          };
+        };
+        msmtp.enable = true;
+        mu.enable = true;
+        imap = {
+          host = "imap.gmail.com";
+          port = 993;
+          tls.enable = true;
+        };
+        smtp = {
+          host = "smtp.gmail.com";
+          port = 465;
+          tls.enable = true;
         };
       };
     };
